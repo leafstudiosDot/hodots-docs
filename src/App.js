3f2f36ed6e3865b35af4d5e8692647b41c50bc0b
aliamanuba <31815005+TuxPenguin09@users.noreply.github.com>
2021-10-04 00:15:09 +0800

    Preparing Home stuffs lesgo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import './App.css';
 import marked from "marked";
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          {Header()}
+        </header>
+        <Switch>
+          <Route exact path={"/"}>
+            {Home()}
+          </Route>
+        </Switch>
+      </div>
+    )
+  }
+}
+
+function Home() {
+  return(
+    <div className="Page">
+
     </div>
-  );
+  )
+}
+
+function Header() {
+  return (
+    <div className="Header">
+
+    </div>
+  )
 }
 
 export default App;
