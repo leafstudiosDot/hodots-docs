34050886ce0f349d7fd11f433f203cbeb9a39bb4
aliamanuba <31815005+TuxPenguin09@users.noreply.github.com>
2021-10-04 06:59:10 +0800

    Markdown File Import success

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 import './App.css';
 import marked from "marked";
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { Route, Switch, Link } from "react-router-dom";
-import {ReactComponent as WebLogo} from "./assets/icon.svg"
+import { ReactComponent as WebLogo } from "./assets/icon.svg"
+import homemd from './docs/home.md'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header>
-          {Header()}
-        </header>
-        <span style={{top: "60px", position: "absolute"}}>
+function App() {
+  return (
+    <div className="App">
+      <header>
+        {Header()}
+      </header>
+      <span style={{ top: "60px", position: "absolute" }}>
         <Switch>
           <Route exact path={"/"}>
             {Home()}
           </Route>
         </Switch>
-        </span>
-      </div>
-    )
-  }
+      </span>
+    </div>
+  )
 }
 
 function Home() {
-  return(
+  const [markdown, setMarked] = useState("");
+
+  useEffect(() => {
+    fetch(homemd)
+      .then(response => {
+        return response.text()
+      })
+      .then(text => {
+        //console.log(text)
+        setMarked(marked(text))
+      })
+  });
+
+  return (
     <div className="Page">
-      
+      <section>
+        <article dangerouslySetInnerHTML={{ __html: markdown }}></article>
+      </section>
     </div>
   )
 }
---
+++ src/docs/home.md
@@ -0,0 +1,4 @@
+# Welcome!
+This is a welcome page of hodots. Docs for Developers
+## hodots. Docs for Developers and what does it do?
+It would show documentations for you (developers outside hodots.) and your application to have instructions on accessing the hodots. Services.
